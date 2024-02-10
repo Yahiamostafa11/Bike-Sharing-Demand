0b80965ceeb48f077969ffff1fe92ef96f345de7
Yahiamostafa11 <yahiawork@gmail.com>
2024-02-10 10:50:46 -0800

    stamony

---
+++ lip.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#importing the dataset
+
+train = pd.read_csv('')
+teast = pd.read_csv('')
